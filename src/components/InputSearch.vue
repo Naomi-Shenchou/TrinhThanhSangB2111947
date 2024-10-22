1c0729fcb1b4452aae8e75f506fa17ff354bf40e
Naomi <trithanhsang203@gmail.com>
2024-10-22 15:03:23 +0700

    Create a contact listing page

--- src/components/InputSearch.vue
+++ src/components/InputSearch.vue
@@ -1,8 +1,14 @@
+<script setup>
+    const model = defineModel({
+        type: String,
+        default: '',
+    });
+</script>
 <template>
-  <div class="input-group">
-    <input type="text" class="form-control px-3" placeholder="Nhập thông tin cần tìm" />
-    <button class="btn btn-outline-secondary" type="button">
-      <i class="fas fa-search"></i>
-    </button>
-  </div>
+    <div class="input-group">
+        <input type="text" class="form-control px-3" placeholder="Nhập thông tin cần tìm" v-model="model" />
+        <button class="btn btn-outline-secondary" type="button" @click="$emit('submit')">
+            <i class="fas fa-search"></i>
+        </button>
+    </div>
 </template>
